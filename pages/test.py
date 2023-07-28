1a7e2b85333498697b91a04d641a8a027cc32908
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-07-28 10:38:40 +0100

    Update test.py path to df

--- pages/test.py
+++ pages/test.py
@@ -8,6 +8,8 @@ st.sidebar.title('Your Data:')
 linear_model = joblib.load('models/linear_model.joblib')
 class_preproc = joblib.load('models/logit_preproc.joblib')
 class_model = joblib.load('models/logit_xgboost_model.joblib')
+# Categorical variables
+df = pd.read_csv('treated_df.csv')
 
 mock = 'Select an option'
 
@@ -73,8 +75,7 @@ with st.sidebar:
     st.subheader(f':white_check_mark: Balance: {balance}')
 
 
-# Categorical variables
-df = pd.read_csv('../treated_df.csv')
+
 
 # term
 st.markdown("### What is the desired timeframe for your payment?")
